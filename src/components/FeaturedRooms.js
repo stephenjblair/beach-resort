5e6d2ae1b5b5aafe405cb1f3f14b583cb9e2f33e
Stephen Blair <stephenjohnblair@outlook.com>
2020-11-24 14:04:25 +0000

    Imported items from data and set state

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { RoomContext } from '../context';
+
+
+
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext
+    render() {
+        const { featuredRooms: rooms } = this.context
+        console.log(rooms)
+        return (
+            <div>
+                 from featured rooms 
+            </div>
+        )
+    }
+}
