527195776570c3fb13201b487572ce919b2c01af
Stephen Blair <stephenjohnblair@outlook.com>
2020-11-24 14:13:24 +0000

    Added Loading and Room basic components

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
 
 
 
@@ -11,6 +13,8 @@ export default class FeaturedRooms extends Component {
         return (
             <div>
                  from featured rooms 
+                 <Room />
+                 <Loading />
             </div>
         )
     }
---
+++ src/components/Room.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Room = () => (
+    <p>Hello from room</p>
+)
+
+export default Room;
