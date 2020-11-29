a52fdba226c0a5e017224972ef86899decb56f89
Stephen Blair <stephenjohnblair@outlook.com>
2020-11-29 16:10:56 +0000

    Amended state in context for RoomsFilter

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title  from '../components/Title';
 
 const RoomsFilter = () => {
+    const context = useContext(RoomContext)
+    console.log(context)
     return (
     <div>hello from rooms filter</div>
     )
