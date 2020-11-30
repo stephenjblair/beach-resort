50cbbfcb1c4cf08de9b3ad6e5a589748b4b12039
Stephen Blair <stephenjohnblair@outlook.com>
2020-11-30 18:45:59 +0000

    Added price range input to filter form

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -42,6 +42,14 @@ const RoomsFilter = ({ rooms }) => {
                     </select>
                 </div>
                 {/*end guests */}
+                {/*room price*/}
+                <div className="form-group">
+                    <label htmlFor="price">
+                        room price £{price}
+                    </label>
+                    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control"/>
+                </div>
+                {/*end of price*/}
             </form>
         </section>
     )
